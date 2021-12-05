d0fd1235a06f04f31cf4b8523d9d8b3208165c0e
zxccxccxz <galexoks@gmail.com>
2021-12-05 14:06:21 +0300

    acm & physics

--- src/physics.java
+++ src/physics.java
@@ -1,9 +1,26 @@
-import
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+import java.awt.*;
+import java.awt.event.*;
+import acm.util.RandomGenerator;
 
 public class physics
 {
     public class ball
     {
+        private double posX;
+        private double posY;
+
+        private GOval ballInstance;
+
+        public ball(double posX, double posY)
+        {
+            this.posX = posX;
+            this.posY = posY;
+            ballInstance = new GOval(20, 20, posX, posY);
+        }
+
 
     }
 }
