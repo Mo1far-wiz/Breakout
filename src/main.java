b62c3d95b810356da1f3af6e407f196af90802fe
Molfar <sasha.prohor12@gmail.com>
2021-12-05 14:05:11 +0200

    added run()

--- src/main.java
+++ src/main.java
@@ -1,2 +1,8 @@
-public class main {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class main extends GraphicsProgram {
+    public void run(){
+        this.setSize(600, 1200);
+    }
 }
