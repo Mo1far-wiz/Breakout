a26c37772b8700af462a41d87246c6741eb8e1c2
zxccxccxz <galexoks@gmail.com>
2021-12-05 14:16:57 +0300

    balls

--- src/main.java
+++ src/main.java
@@ -1,7 +1,18 @@
-public class main
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+import java.awt.*;
+import java.awt.event.*;
+import acm.util.RandomGenerator;
+
+public class main extends GraphicsProgram
 {
+    private physics physics = new physics();
+
     public void run()
     {
-        int a = 1 / 5;
+        this.setSize(600, 800);
+
+        physics.createBall(200, 200);
     }
 }
--- src/physics.java
+++ src/physics.java
@@ -5,8 +5,21 @@ import java.awt.*;
 import java.awt.event.*;
 import acm.util.RandomGenerator;
 
-public class physics
+public class physics extends GraphicsProgram
 {
+    private ball ball;
+
+    public void createBall(double posX, double posY)
+    {
+        ball = new ball(posX, posY);
+    }
+
+    public ball getBall()
+    {
+        return ball;
+    }
+
+
     public class ball
     {
         private double posX;
@@ -18,9 +31,11 @@ public class physics
         {
             this.posX = posX;
             this.posY = posY;
-            ballInstance = new GOval(20, 20, posX, posY);
+            ballInstance = new GOval(20, 20);
+            ballInstance.setFilled(true);
+            ballInstance.setColor(Color.RED);
+            ballInstance.setLocation(posX, posY);
+            add(ballInstance);
         }
-
-
     }
 }
