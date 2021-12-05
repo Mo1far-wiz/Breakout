68e8f50398fec6a9983c1daa54c58c31cbcc2224
zxccxccxz <galexoks@gmail.com>
2021-12-05 18:39:00 +0300

    bigball v4

--- src/DrawGraphics.java
+++ src/DrawGraphics.java
@@ -1,10 +1,24 @@
-import acm.program.GraphicsProgram;
+import acm.graphics.GRect;
+import acm.graphics.GRoundRect;
 
-public class DrawGraphics extends GraphicsProgram {
-    private double brick_width = 0;
-    private double brick_height = 0;
+import java.awt.*;
 
-    public void drawBrick(){
-
+public class DrawGraphics extends main {
+    public GRoundRect setBrick(double x, double y, int nrow){
+        GRoundRect brick = new GRoundRect(getBRICK_WIDTH(), getBRICK_HEIGHT());
+        nrow /= 2;
+        brick.setFilled(true);
+        brick.setLocation(x, y);
+        if(nrow == 0)
+            brick.setColor(Color.decode("#d50000"));
+        else if(nrow == 1)
+            brick.setColor(Color.decode("#f57f17"));
+        else if(nrow == 2)
+            brick.setColor(Color.decode("#ffea00"));
+        else if(nrow == 3)
+            brick.setColor(Color.decode("#558b2f"));
+        else if(nrow == 4)
+            brick.setColor(Color.decode("#00b8d4"));
+        return brick;
     }
 }
